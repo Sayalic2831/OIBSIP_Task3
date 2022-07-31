03021c7b0d2102d74148a23354a678eccd503079
Sayalic2831 <98662059+Sayalic2831@users.noreply.github.com>
2022-07-31 18:46:25 +0530

    Add files via upload

---
+++ ATM_Interface.java
@@ -0,0 +1,8 @@
+package Task_3;
+
+public interface ATM_Interface {
+    public void viewBalance();
+    public void withdrawnAmount(double withdrawnAmot);
+    public void depositAmount(double depositAmt);
+    public void viewtansactionHistory();
+}
